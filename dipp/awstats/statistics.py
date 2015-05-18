66ab9306192ff58905ad3a588d6360ccb5bfa8ad
Peter Reimer <reimer@hbz-nrw.de>
2015-05-18 15:58:07 +0200

    add method which returns just the list of parsed month as list

--- dipp/awstats/statistics.py
+++ dipp/awstats/statistics.py
@@ -1,11 +1,36 @@
 #!/usr/bin/env python
 from bda.awstatsparser.parser import ParsedStatistics
 
-parser = ParsedStatistics('afrika','/files/var/awstats/afrika')
+class Statistics:
+    
+    def __init__(self, year, journal, urls):
+        
+        self.journal = journal
+        self.year = year
+        self.urls = urls
+        self.parser = ParsedStatistics(self.journal, '/files/var/awstats/' + self.journal)
+        
+    def available_years(self):
+        years = [int(y[2:6]) for y in self.parser.available]
+        return list(set(years))
 
-for month in parser.available:
-    print month
-    sider = parser[month].get('SIDER',None)
-    if sider:
-        stat = sider.get('/archiv/2013/3754/', None)
-        print stat
+    def get_data(self):
+        
+        months = ["%02d%s" % (m, self.year) for m in range(1, 13 )]
+        for month in months:
+            if month in self.parser.available:
+                try:
+                    sider = self.parser[month].get('SIDER',None)
+                    for url in self.urls:
+                        stat = sider.get(url, None)
+                except:
+                    stat = ":("
+            print month, stat
+             
+
+if __name__ == '__main__':
+    
+    urls = ['/archiv/2013/3754/']
+    stats = Statistics(2014, "afrika", urls)
+    print stats.available_years()
+    #print stats.get_data()
