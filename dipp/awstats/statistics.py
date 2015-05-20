ee14d1718f4a59c98786c6c083a4c7581dfb2d65
Peter Reimer <reimer@hbz-nrw.de>
2015-05-20 16:44:29 +0200

    added logging

--- dipp/awstats/statistics.py
+++ dipp/awstats/statistics.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python
 from bda.awstatsparser.parser import ParsedStatistics
+import os
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+console = logging.StreamHandler()
+formatter = logging.Formatter('%(levelname)s - %(message)s')
+console.setFormatter(formatter)
+logger.addHandler(console)
 
 AWSTATS_DIR = '/files/var/awstats/'
 
@@ -8,7 +17,12 @@ class Statistics:
     def __init__(self, journal):
         
         self.journal = journal
-        self.parser = ParsedStatistics(self.journal, AWSTATS_DIR + self.journal)
+        data_dir = AWSTATS_DIR + self.journal
+        if not os.path.isdir(data_dir):
+            logger.error("Directory %s does not exist" % data_dir)
+        logger.info("Parsing %s" % data_dir)
+        self.parser = ParsedStatistics(self.journal, data_dir )
+        self.available = self.parser.available
         
     def available_years(self):
         """Return a list of years (int) for which data are available """
@@ -22,7 +36,7 @@ class Statistics:
         return ["%02d%s" % (m, year) for m in range(1, 13)]
 
     def get_data(self, year, urls):
-        """return the access statistics of a given url an year"""
+        """return the access statistics of a given url and year"""
         
         months = self.months(year)
         entries = {}
@@ -30,23 +44,32 @@ class Statistics:
             entries[url] = dict.fromkeys(months)
 
         for month in months:
-            if month in self.parser.available:
-                sider = self.parser[month].get('SIDER',None)
-                for url in urls:
-                    stats = sider.get(url, None)
-                    if stats:
-                        entries[url][month] = int(stats.get('entry', 0))
-                    else:
-                        entries[url][month] = 0
+            if month in self.available:
+                # probably not the most elegant way, but sometimes getting parser[month]
+                # fails although it is in self.available   
+                try:
+                    month_parser = self.parser[month]
+                except:
+                    month_parser = {}
+             
+                sider = month_parser.get('SIDER',None)
+                #sider = self.parser[month].get('SIDER',None)
+                if sider:
+                    for url in urls:
+                        stats = sider.get(url, None)
+                        if stats:
+                            entries[url][month] = int(stats.get('entry', 0))
+                        else:
+                            entries[url][month] = 0
         return entries
 
 if __name__ == '__main__':
     
-    urls = ['/archiv/2010/2723/dippArticle.xml']#, '/archiv/2010/2723/SomaliWritingsPDF','/archiv/2010/2723']
+    #urls = ['/archiv/2010/2723/dippArticle.xml']#, '/archiv/2010/2723/SomaliWritingsPDF','/archiv/2010/2723']
     #urls = ['/archiv/2010/2723/SomaliWritingsPDF','/archiv/2010/2723']
-    #urls = ['/archiv/2010/2723']
+    urls = ['/archiv/2010/2723']
     stats = Statistics('afrika')
-    
-    data =  stats.get_data(2013, urls)
-    for url in stats.get_data(2013, urls).keys():
+    year = 2015
+    data =  stats.get_data(year, urls)
+    for url in stats.get_data(year, urls).keys():
         print url, data[url] 
